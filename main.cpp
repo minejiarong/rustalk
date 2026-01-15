9470815dd8b495b44c703ad65fc8661da53534ea
robai <robaix@eicloud.com>
2026-01-15 22:14:55 +0800

    Update main.cpp to call Rust function and fix lib.rs syntax

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
-#include "mainwindow.h"
-
-#include <QApplication>
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
-}
+#include "mainwindow.h" 
+ #include <QApplication> 
+ #include <QDebug> 
+ 
+ extern "C" { 
+     int rustalk_add(int a, int b); 
+ } 
+ 
+ int main(int argc, char *argv[]) 
+ { 
+     QApplication a(argc, argv); 
+ 
+     int result = rustalk_add(2, 3); 
+     qDebug() << "rustalk_add(2,3) =" << result; 
+ 
+     MainWindow w; 
+     w.show(); 
+ 
+     return a.exec(); 
+ }
--- rust/rustalk_core/src/lib.rs
+++ rust/rustalk_core/src/lib.rs
@@ -1,4 +1,4 @@
-#[unsafe(no_mangle)]
+#[no_mangle]
 pub extern "C" fn rustalk_add(a: i32, b: i32) -> i32 {
     a + b
 }
